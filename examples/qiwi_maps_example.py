fd7fdaec6c7c4fc5a4d930fc36ab76e621432c2a
Gleb <71976818+GLEF1X@users.noreply.github.com>
2021-04-23 20:14:21 +0300

    Update qiwi_maps_example.py

--- examples/qiwi_maps_example.py
+++ examples/qiwi_maps_example.py
@@ -1,19 +1,24 @@
-"""pip install geopy"""
 import asyncio
+
 from geopy.distance import distance
 
 from glQiwiApi import QiwiMaps
 from glQiwiApi.types import Polygon
+from glQiwiApi.utils.basics import measure_time
 
 
+@measure_time
 async def qiwi_maps():
     my_lat_lon = (55.719384, 37.781102)
     polygon = Polygon((55.690881, 37.386282), (55.580184, 37.826078))
 
     maps = QiwiMaps()
     async with maps:
-        terminals = await maps.terminals(polygon, zoom=12,
-                                         cache_terminals=True)
+        terminals = await maps.terminals(
+            polygon,
+            zoom=12,
+            cache_terminals=True
+        )
 
     for terminal in terminals:
         terminal_latlon = (
