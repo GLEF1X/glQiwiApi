bed3deabb1831b95e298c671314b39cc2b40d5d5
Gleb <71976818+GLEF1X@users.noreply.github.com>
2021-04-24 21:35:31 +0300

    Update qiwi_webhook.py

--- examples/qiwi/qiwi_webhook.py
+++ examples/qiwi/qiwi_webhook.py
@@ -3,6 +3,7 @@ import logging
 from aiogram import Bot
 
 from glQiwiApi import QiwiWrapper, types
+from glQiwiApi.core.web_hooks.config import Path
 
 TOKEN = 'token from https://qiwi.com/api/'
 QIWI_SECRET = 'secret token from https://qiwi.com/p2p-admin/'
@@ -29,6 +30,22 @@ async def main2(event: types.Notification):
 
 # Custom format for logging
 FORMAT = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+
+# Also, you can specify a path for webhook
+# Example: http://127.0.0.1/your_path/
+# If you don't pass path in `start_webhook`
+# or dont pass on transaction_path or bill_path
+# Its ok, because it will take a default paths
+# default transaction_path = /web_hooks/qiwi/
+# default bill_path = /webhooks/qiwi/bills/
+# So, if you dont pass on paths
+# you need to register webhook with url like
+# on this example: http://your_ip:port/web_hooks/qiwi/ - for transactions
+# or http://your_ip:port/webhooks/qiwi/bills/ - for bills
+path = Path(
+    transaction_path="/web_hooks/qiwi",
+    bill_path="/my_webhook/"
+)
 
 wallet.start_webhook(
     # You can pass on any port, but it must be open for web
