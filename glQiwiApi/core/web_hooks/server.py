ddb49dfabf13ab219a576f74e0b74d1eee3c3d8e
Gleb Garanin <glebgar567@gmail.com>
2021-05-11 20:07:39 +0300

    Add on_shutdown and on_startup callbacks for polling and webhook mode

--- glQiwiApi/core/web_hooks/server.py
+++ glQiwiApi/core/web_hooks/server.py
@@ -7,7 +7,7 @@ from aiohttp.web_response import Response
 
 from glQiwiApi import types
 from glQiwiApi.core.abstracts import BaseWebHookView
-from glQiwiApi.core.web_hooks.handler import Dispatcher
+from glQiwiApi.core.web_hooks.dispatcher import Dispatcher
 from glQiwiApi.utils.basics import hmac_for_transaction, hmac_key
 from .config import (
     DEFAULT_QIWI_WEBHOOK_PATH,
@@ -47,14 +47,16 @@ class QiwiWalletWebView(BaseWebHookView):
         data = await self.request.json()
         return types.WebHook.parse_raw(data)
 
-    async def post(self) -> Response:
+    async def post(self) -> web.Response:
         await super().post()
         return web.Response(text="ok", status=200)
 
-    def _hash_validator(self, update: types.WebHook) -> None:
+    def _hash_validator(self, update: typing.Union[
+        types.Notification, types.WebHook
+    ]) -> None:
         base64_key = self.request.app.get('_base64_key')
 
-        if update.test:
+        if not update.payment:
             return
 
         validated = hmac_for_transaction(
@@ -83,7 +85,9 @@ class QiwiBillWebView(BaseWebHookView):
     def _check_ip(self, ip_address: str) -> bool:
         return _check_ip(ip_address)
 
-    def _hash_validator(self, update: types.Notification) -> None:
+    def _hash_validator(self, update: typing.Union[
+        types.Notification, types.WebHook
+    ]) -> None:
         sha256_signature = self.request.headers.get("X-Api-Signature-SHA256")
         logging.info(sha256_signature)
         _secret = self.request.app.get("_secret_key")
@@ -94,7 +98,9 @@ class QiwiBillWebView(BaseWebHookView):
         if answer != sha256_signature:
             raise web.HTTPBadRequest()
 
-    async def parse_update(self) -> types.Notification:
+    async def parse_update(self) -> typing.Union[
+        types.Notification, types.WebHook
+    ]:
         payload = await self.request.json()
         return types.Notification.parse_raw(payload)
 
@@ -103,6 +109,7 @@ class QiwiBillWebView(BaseWebHookView):
 
         update = await self.parse_update()
 
+        # Validation is still in development
         # self._hash_validator(update)
 
         await self.handler_manager.process_event(update)
@@ -114,50 +121,97 @@ class QiwiBillWebView(BaseWebHookView):
 
 def setup_transaction_data(
         app: web.Application,
-        base64_key: str,
+        base64_key: typing.Optional[str],
         handler_manager: Dispatcher,
-        path: typing.Optional[str] = None
+        path: typing.Optional[Path] = None
 ):
     app["_base64_key"] = base64_key
     app[QiwiWalletWebView.app_key_check_ip] = _check_ip
     app[QiwiWalletWebView.app_key_handler_manager] = handler_manager
-    path = path or DEFAULT_QIWI_WEBHOOK_PATH
-    app.router.add_view(path, QiwiWalletWebView, name=DEFAULT_QIWI_ROUTER_NAME)
+    if isinstance(path, Path):
+        path = path.transaction_path
+    else:
+        path = DEFAULT_QIWI_WEBHOOK_PATH
+    app.router.add_view(
+        path,
+        QiwiWalletWebView,
+        name=DEFAULT_QIWI_ROUTER_NAME
+    )
 
 
 def setup_bill_data(
         app: web.Application,
-        secret_key: str,
+        secret_key: typing.Optional[str],
         handler_manager: Dispatcher,
-        path: typing.Optional[str] = None
+        path: typing.Optional[Path] = None
 ) -> None:
     app["_secret_key"] = secret_key
     app[QiwiBillWebView.app_key_check_ip] = _check_ip
     app[QiwiBillWebView.app_key_handler_manager] = handler_manager
+    if isinstance(path, Path):
+        path: str = path.bill_path
+    else:
+        path: str = DEFAULT_QIWI_BILLS_WEBHOOK_PATH
     app.router.add_view(
         handler=QiwiBillWebView,
         name=DEFAULT_QIWI_BILLS_ROUTER_NAME,
-        path=path or DEFAULT_QIWI_BILLS_WEBHOOK_PATH
+        path=path
     )
 
 
 def setup(
-        handler_manager: Dispatcher,
+        dispatcher: Dispatcher,
         app: web.Application,
+        instance: typing.Any,
+        on_startup: typing.Optional[
+            typing.Callable[[web.Application], typing.Awaitable[None]
+            ]],
+        on_shutdown: typing.Optional[
+            typing.Callable[
+                [web.Application], typing.Awaitable[None]
+            ]],
         path: typing.Optional[Path] = None,
         secret_key: typing.Optional[str] = None,
-        base64_key: typing.Optional[str] = None
+        base64_key: typing.Optional[str] = None,
 ) -> None:
     """
     Setup web application for webhooks
 
-    :param handler_manager: handler_manager, which processing events
+    :param dispatcher: dispatcher, which processing events
     :param app: aiohttp.web.Application
     :param path: Path obj, contains two paths
     :param secret_key: secret p2p key
     :param base64_key: Base64-encoded webhook key
+    :param on_startup: coroutine,which will be executed on startup
+    :param on_shutdown: coroutine, which will be executed on shutdown
+    :param instance: instance of the QiwiWrapper
     """
-    setup_bill_data(app, secret_key, handler_manager,
-                    path.bill_path)
-    setup_transaction_data(app, base64_key, handler_manager,
-                           path.transaction_path)
+
+    setup_bill_data(app, secret_key, dispatcher,
+                    path)
+    setup_transaction_data(app, base64_key, dispatcher,
+                           path)
+    _setup_callbacks(on_startup, on_shutdown, instance, app)
+
+
+def _setup_callbacks(
+        on_startup: typing.Optional[
+            typing.Callable[[web.Application], typing.Awaitable[None]]
+        ],
+        on_shutdown: typing.Optional[
+            typing.Callable[[web.Application], typing.Awaitable[None]]
+        ],
+        instance: typing.Any,
+        app: web.Application
+) -> None:
+    """
+    Function, which deals with on_startup and on_shutdown functions
+
+    :param on_startup: coroutine,which will be executed on startup
+    :param on_shutdown: coroutine, which will be executed on shutdown
+    :param instance: instance of the QiwiWrapper
+    :param app: instance of aiohttp.web.Application()
+    """
+    app["qiwi_wrapper"] = instance
+    app.on_startup.append(on_startup)
+    app.on_shutdown.append(on_shutdown)
