024b97b89dcb93094a917302e6d3ce8f7ac16815
Gleb Garanin <glebgar567@gmail.com>
2021-05-13 16:55:51 +0300

    ✔Add new pydantic model `Card`

--- glQiwiApi/types/qiwi_types/qiwi_master.py
+++ glQiwiApi/types/qiwi_types/qiwi_master.py
@@ -1,4 +1,5 @@
-from typing import Optional
+import datetime
+from typing import Optional, List
 
 from pydantic import BaseModel, Field
 
@@ -14,6 +15,63 @@ class OrderDetails(BaseModel):
     card_id: Optional[str] = Field(alias="cardId", default=None)
 
 
+class CardCredentials(BaseModel):
+    """object: CardCredentials"""
+    qvx_id: int = Field(..., alias="id")
+    masked_pan: str = Field(..., alias="maskedPan")
+    status: str
+    card_expire: datetime.datetime = Field(..., alias="cardExpire")
+    card_type: str = Field(..., alias="cardType")
+    card_alias: Optional[str] = Field(..., alias="cardAlias")
+    activated: datetime.datetime
+    sms_recender: Optional[datetime.datetime] = Field(..., alias="smsResended")
+    post_number: Optional[str] = Field(default=None, alias="postNumber")
+    blocked_date: Optional[datetime.datetime] = Field(default=None,
+                                                      alias="blockedDate")
+    card_id: int = Field(..., alias="cardId")
+    txn_id: int = Field(..., alias="txnId")
+    card_expire_month: str = Field(..., alias="cardExpireMonth")
+    card_expire_year: str = Field(..., alias="cardExpireYear")
+
+
+class Requisite(BaseModel):
+    """object: Requisite"""
+    name: str
+    value: str
+
+
+class Details(BaseModel):
+    """object: Details"""
+    info: str
+    description: str
+    tariff_link: str = Field(..., alias="tariffLink")
+    offer_link: str = Field(..., alias="offerLink")
+    features: List[str]
+    requisites: List[Requisite]
+
+
+class CardInfo(BaseModel):
+    """object: CardInfo"""
+    id_: int = Field(..., alias="id")
+    name: str
+    alias: str
+    price: Sum
+    period: str
+    type_: str = Field(..., alias="type")
+    details: Details
+
+
+class Card(BaseModel):
+    """
+    object: Card
+    description: Данные выпущенных карт
+    """
+    details: CardCredentials = Field(..., alias="qvx")
+    balance: Optional[Sum] = None
+    info: CardInfo
+
+
 __all__ = (
-    'OrderDetails'
+    'OrderDetails',
+    'Card'
 )
