51e2d69afc6b44aa11afbd6af6c8b2616af7ab27
Gleb Garanin <glebgar567@gmail.com>
2021-06-02 22:47:59 +0300

    🔥 Add base class for types

---
+++ glQiwiApi/types/base.py
@@ -0,0 +1,24 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from pydantic import BaseModel
+
+if TYPE_CHECKING:
+    from glQiwiApi.qiwi.client import QiwiWrapper
+
+
+class Base(BaseModel):
+
+    @property
+    def client(self) -> QiwiWrapper:
+        """ Returning an instance of :class:`QiwiWrapper` """
+        from glQiwiApi.qiwi.client import QiwiWrapper
+
+        instance = QiwiWrapper.get_current()
+
+        if instance is None:
+            raise RuntimeError("Can't get client instance from context. "
+                               "You can fix it with setting current instance: "
+                               "'QiwiWrapper.set_current(wrapper_instance)'")
+        return instance
