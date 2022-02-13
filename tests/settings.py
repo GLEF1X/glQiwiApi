078b442f58c9dfe904fcea4315fd4774a71e751b
Gleb Garanin <glebgar567@gmail.com>
2022-02-13 18:26:13 +0200

    :bug: Fix CI

--- tests/settings.py
+++ tests/settings.py
@@ -1,11 +1,11 @@
 import os
 
 QIWI_WALLET_CREDENTIALS = {
-    "phone_number": os.getenv("PHONE_NUMBER"),
-    "api_access_token": os.getenv("API_ACCESS_TOKEN"),
+    "phone_number": os.getenv("QIWI_PHONE_NUMBER"),
+    "api_access_token": os.getenv("QIWI_API_ACCESS_TOKEN"),
 }
 
-QIWI_P2P_CREDENTIALS = {"secret_p2p": os.getenv("SECRET_P2P")}
+QIWI_P2P_CREDENTIALS = {"secret_p2p": os.getenv("QIWI_SECRET_P2P")}
 
 YOO_MONEY_CREDENTIALS = {"api_access_token": os.getenv("YOOMONEY_API_TOKEN")}
 
