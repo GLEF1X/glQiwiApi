811b4d67c684fc48daad05653872a7640c533004
GLEF1X <glebgar567@gmail.com>
2022-04-09 22:20:38 +0300

    fix 2.0.5 release problem

--- glQiwiApi/core/abc/base_api_client.py
+++ glQiwiApi/core/abc/base_api_client.py
@@ -13,7 +13,7 @@ from typing import (
     Dict,
     Callable,
     Awaitable,
-    Union,
+    Union, Tuple,
 )
 
 if MYPY:
@@ -25,11 +25,49 @@ RequestServiceFactoryType = Callable[
     ..., Union[Awaitable["RequestServiceProto"], "RequestServiceProto"]
 ]
 
+_C = TypeVar("_C", bound=Type["APIClientMeta"])
 
-class BaseAPIClient(abc.ABC):
+
+class APIClientMeta(abc.ABCMeta):
+    """
+    This metaclass fixes issue connected with poor release
+    of glQiwiApi 2.0.5 that breaks all down. It's a complicated workaround of building of request service
+    "on-flight" and allows users of API client to use custom factories of a request service.
+
+    Due to limitations of aiohttp library that requires to create ClientSession only inside coroutine
+    I have to write this metaclass to avoid additional viscous boilerplate code.
+    """
+
+    def __new__(mcs: Type[_C], name: str, bases: Tuple[Any, ...], attrs: Dict[str, Any], **kwargs: Any) -> _C:
+        for key, attribute in attrs.items():
+            is_name_mangled = key.startswith("__")
+            if is_name_mangled:
+                continue
+
+            if not inspect.iscoroutinefunction(attribute):
+                continue
+
+            if key in ("close", "_create_request_service", "create_request_service"):
+                continue
+
+            def wrapper(m) -> Any:
+                async def check_request_service_before_execute(self, *args: Any, **kw: Any) -> Any:
+                    if self._request_service is None:
+                        self._request_service = await self.create_request_service()
+
+                    return await m(self, *args, **kw)
+
+                return check_request_service_before_execute
+
+            attrs[key] = wrapper(attrs[key])
+
+        return super().__new__(mcs, name, bases, attrs, **kwargs)
+
+
+class BaseAPIClient(metaclass=APIClientMeta):
     def __init__(
-        self,
-        request_service_factory: Optional[RequestServiceFactoryType] = None,
+            self,
+            request_service_factory: Optional[RequestServiceFactoryType] = None,
     ):
         self._request_service_factory = request_service_factory
         self._request_service: RequestServiceProto = None  # type: ignore
@@ -40,10 +78,10 @@ class BaseAPIClient(abc.ABC):
         return self
 
     async def __aexit__(
-        self,
-        exc_type: Optional[Type[BaseException]],
-        exc_value: Optional[BaseException],
-        traceback: Optional[TracebackType],
+            self,
+            exc_type: Optional[Type[BaseException]],
+            exc_value: Optional[BaseException],
+            traceback: Optional[TracebackType],
     ) -> None:
         await self.close()
 
