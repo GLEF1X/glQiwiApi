23cdd2e7a2f66a4c8c20efa979bed63a348c66cb
Gleb Garanin <glebgar567@gmail.com>
2022-07-07 14:19:10 +0200

    [bugfix]: replace custom `__hash__` and `__eq__` with pydantic's `frozen=True`

--- glQiwiApi/types/base.py
+++ glQiwiApi/types/base.py
@@ -15,9 +15,4 @@ class Base(BaseModel):
 class HashableBase(Base):
     class Config(BaseConfig):
         allow_mutation = False
-
-    def __hash__(self) -> int:
-        return hash((type(self),) + tuple(self.__dict__.values()))
-
-    def __eq__(self, other: Hashable) -> bool:
-        return self.__hash__() == other.__hash__()
+        frozen = True
