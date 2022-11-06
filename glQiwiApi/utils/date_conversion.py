8bd6e5824e306753ab10ddee244d24a843c5a227
GLEF1X <glebgar567@gmail.com>
2022-11-05 22:56:01 -0400

    :ambulance: #89 date conversion bugfix and improvements

--- glQiwiApi/utils/date_conversion.py
+++ glQiwiApi/utils/date_conversion.py
@@ -1,14 +1,31 @@
-from datetime import datetime
+import sys
+from datetime import datetime, timezone
 from typing import Optional
-
-import pytz
 
 DEFAULT_QIWI_TIMEZONE = 'Europe/Moscow'
 
+if sys.version_info < (3, 9, 0):
+    try:
+        import pytz
+    except ImportError:
+        raise RuntimeError(
+            'glQiwiApi requires `pytz` package for python < 3.9'
+            'as an alternative to a new builtin `zoneinfo`. '
+            'Please install pytz using pip install pytz.'
+        )
+
+    def to_moscow(obj: datetime) -> datetime:
+        return obj.astimezone(pytz.timezone(DEFAULT_QIWI_TIMEZONE))
+
+else:
+    import zoneinfo
+
+    def to_moscow(obj: datetime) -> datetime:
+        return obj.astimezone(zoneinfo.ZoneInfo(DEFAULT_QIWI_TIMEZONE))
+
 
 def datetime_to_utc_in_iso_format(obj: datetime) -> str:
-    iso_format_date = pytz.utc.localize(obj).replace(tzinfo=None).isoformat(' ')
-    return f'{iso_format_date.replace(" ", "T")}Z'
+    return obj.astimezone(tz=timezone.utc).isoformat(timespec='milliseconds')
 
 
 def datetime_to_iso8601_with_moscow_timezone(obj: Optional[datetime]) -> str:
@@ -20,4 +37,4 @@ def datetime_to_iso8601_with_moscow_timezone(obj: Optional[datetime]) -> str:
     """
     if not isinstance(obj, datetime):
         return ''  # pragma: no cover
-    return obj.astimezone(pytz.timezone(DEFAULT_QIWI_TIMEZONE)).isoformat(timespec='seconds')
+    return to_moscow(obj).isoformat(timespec='seconds')
